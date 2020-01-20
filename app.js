91ee50e4453197cf1f60882c3c8cb9e6814cf2cb
Mal <malrsteffes@gmail.com>
2020-01-20 18:20:25 -0500

    income and expense constructors added

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 // BUDGET CONTROLLER ===================
-
+// Immediately Invoked Function Expression keeps data private
 let budgetController = (function() {
-  // Some code
+  // Expense Function Constructor
+  let Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  // Income Function Constructor
+  let Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  // Store new data items in respective arrays
+  let allExpenses = [];
+  let allIncomes = [];
+
+  let data = {
+    allItems: {
+      expenses: [],
+      incomes: []
+    },
+    totals: {
+      expenses: 0,
+      incomes: 0
+    }
+  };
 })();
 
 // UI CONTROLLER ===================
-
+// Immediately Invoked Function Expression keeps data private
 let UIController = (function() {
   // Private variable to hold our dom strings
   let DOMstrings = {
@@ -33,31 +60,45 @@ let UIController = (function() {
 })();
 
 // APP CONTROLLER ===================
-
-// pass the other two modules as arguments to the controller
+// Immediately Invoked Function Expression keeps data private
+// For the app, pass the other two modules as arguments to the controller
 let appController = (function(budgetCtrl, UICtrl) {
-  //Grab DOM strings from the UI Controller
-  let DOM = UICtrl.getDOMstrings();
+  // Function that sets up event listeners
+  let setupEventListeners = function() {
+    //Grab DOM strings from the UI Controller
+    let DOM = UICtrl.getDOMstrings();
+    // Button Event Listener
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+    // Keypress Listener
+    document.addEventListener("keypress", function(event) {
+      // Only run code if button pressed was "enter"
+      // "Which" is for older browsers
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
   // Control add item function that we can pass to both event listeners
   // This way we don't have to write the code twice
   let ctrlAddItem = function() {
     // 1. Get the input data from the user
     let input = UICtrl.getInput();
-    console.log(input);
     // 2. Add the item to the budget controller
     // 3. Add the new item to the UI
     // 4. Calculate the new budget
     // 5. Display the new budget on the UI
   };
 
-  // Button Event Listener
-  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
-  // Keypress Listener
-  document.addEventListener("keypress", function(event) {
-    // Only run code if button pressed was "enter"
-    // "Which" is for older browsers
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
+  // Public initialization function
+  return {
+    init: function() {
+      console.log("The app has started!");
+      setupEventListeners();
     }
-  });
+  };
 })(budgetController, UIController);
+
+// Only line of code placed on the outside. Starts the code.
+
+appController.init();
