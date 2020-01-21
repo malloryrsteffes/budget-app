5d4fc41789f6e96ce3fcabae715d98f4107f3b92
Mal <malrsteffes@gmail.com>
2020-01-20 19:07:19 -0500

    UI is updating semi-correctly

--- app.js
+++ app.js
@@ -68,7 +68,9 @@ let UIController = (function() {
     inputType: ".add__type",
     inputDescription: ".add__description",
     inputValue: ".add__value",
-    inputBtn: ".add__btn"
+    inputBtn: ".add__btn",
+    incomeContainer: ".income__list",
+    expensesContainer: ".expenses__list"
   };
 
   // Function to get our user input
@@ -80,6 +82,28 @@ let UIController = (function() {
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       };
+    },
+    addListItem: function(obj, type) {
+      // 1. Create an HTML with some placeholder text
+      let html, newHtml, element;
+
+      if (type === "inc") {
+        element = DOMstrings.incomeContainer;
+        html =
+          '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else if (type === "exp") {
+        element = DOMstrings.expensesContainer;
+        html =
+          '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+
+      // 2. Replace the placeholder text with real data
+      newHtml = html.replace("%id%", obj.id);
+      newHtml = newHtml.replace("%description%", obj.description);
+      newHtml = newHtml.replace("%value%", obj.value);
+
+      // 3. Insert the HTML into the DOM
+      document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
     },
     // Exposes the DOMstrings to the public
     getDOMstrings: function() {
@@ -120,6 +144,7 @@ let appController = (function(budgetCtrl, UICtrl) {
       input.value
     );
     // 3. Add the new item to the UI
+    UIController.addListItem(newItem, input.type);
     // 4. Calculate the new budget
     // 5. Display the new budget on the UI
   };
